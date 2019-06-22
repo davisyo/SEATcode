189733d64a76474d57d53af929d00638548e1348
davisyo <davidmunozmellado@gmail.com>
2019-06-22 16:24:51 +0200

    renamed variables for better readability

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from read_input import parse_arguments, process_input
 
 def main():
     # Parsing the argument which should be the input file
-    args = parse_arguments()
+    file_name = parse_arguments()
 
     # Create the squad (list of rovers) from the content of the file
-    squad = process_input(args)
+    squad = process_input(file_name)
 
     # Each rover will navigate the plateau and report its location
     for rover in squad:
